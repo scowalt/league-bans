ba456f4a6fdd93da907719e206f6a191d4763ea5
Scott Walters <srwalte2@illinois.edu>
2015-01-29 12:26:08 -0600

    Refactored routes

--- app.js
+++ app.js
@@ -2,44 +2,8 @@
 var express = require('express');
 var app = express();
 
-var lolking = require(__dirname + '/data/lolking');
-var stats = require(__dirname + '/data/stats');
-
 // EXPRESS ROUTES
-app.get('/', function (req, res){
-	lolking.getChampionStatistics(onChampionStatistics);
-
-	function onChampionStatistics(error, data){
-		if (error) {
-			console.error(error);
-			return onError();
-		}
-		stats.computeBanScores(data, onBanScores);
-	}
-
-	function onBanScores(error, data){
-		if (error){
-			console.error(error);
-			return onError();
-		}
-		data.sort(compareChampions);
-		res.status(200).send(data);
-	}
-
-	function compareChampions(a,b){
-		if (a.banScore > b.banScore){
-			return -1;
-		} else if (a.banScore < b.banScore){
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-
-	function onError(){
-		res.status(500).send();
-	}
-});
+app.use(require(__dirname + '/routes'));
 
 // START SERVER
 var server = app.listen(process.env.PORT || 80, function(){
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+var lolking = require(__dirname + '/../data/lolking');
+var stats = require(__dirname + '/../data/stats');
+
+router.get('/', function (req, res){
+	lolking.getChampionStatistics(onChampionStatistics);
+
+	function onChampionStatistics(error, data){
+		if (error) {
+			console.error(error);
+			return onError();
+		}
+		stats.computeBanScores(data, onBanScores);
+	}
+
+	function onBanScores(error, data){
+		if (error){
+			console.error(error);
+			return onError();
+		}
+		data.sort(compareChampions);
+		res.status(200).send(data);
+	}
+
+	function compareChampions(a,b){
+		if (a.banScore > b.banScore){
+			return -1;
+		} else if (a.banScore < b.banScore){
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	function onError(){
+		res.status(500).send();
+	}
+});
+
+module.exports = router;
